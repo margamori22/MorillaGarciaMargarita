968f9f43c7d6002ef381ed15cd42973788966d09
Margarita Morilla García <margamori22@gmail.com>
2026-05-22 18:38:14 +0200

    Primer commit

---
+++ practica-Morilla.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Script de ejemplo para phpDocumentor
+ * 
+ * @author Margarita Morilla García
+ * @version 1.0
+ */
+
+ /**
+ * Función que suma dos números enteros.
+ *
+ * @param int $a Primer número
+ * @param int $b Segundo número
+ * @return int Resultado de la suma
+ */
+function sumar($a, $b) {
+    return $a + $b;
+}
+
+/**
+ * Función que resta dos números enteros.
+ *
+ * @param int $a Primer número
+ * @param int $b Segundo número
+ * @return int Resultado de la resta
+ * @internal Función de ejemplo
+ */
+function restar($a, $b) {
+    return $a - $b;
+}
